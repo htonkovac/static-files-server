b7556f52031eb49dc866aa8fbb772503f5712f7d
htonkovac <htonkovac@gmail.com>
2018-05-20 20:45:21 +0200

    script optimization

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -dit --name my-apache-app -p 80:80 -v /home/tosa/static-files-server:/usr/local/apache2/htdocs/ httpd:2.4
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $DIR
+docker run -d --name my-apache-app -p 80:80 -v ${DIR}:/usr/local/apache2/htdocs/ httpd:2.4
